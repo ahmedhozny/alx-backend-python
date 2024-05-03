d38ddfe8303e8c14eda0a60ffb3255b852e2eabd
Ahmed Hosny <ahmhosny2002@gmail.com>
2024-05-03 05:16:58 +0300

    Docs..

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python3
+"""
+Creates multiple copies of items in a tuple.
+"""
+
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """Creates multiple copies of items in a tuple.
+    """
+    Creates multiple copies of items in a tuple.
     """
     zoomed_in: List = [
         item for item in lst
